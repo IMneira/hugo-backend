c692d15068a3684a3d54d160aee1011940c721e3
IMneira <imneira@miuandes.cl>
2024-10-08 03:32:54 -0300

    agregado serializers.py

---
+++ hugo_api/administrator/serializers.py
@@ -0,0 +1,30 @@
+from models import Curso, Profesor, Seccion, Bloque, Requisito
+from rest_framework import serializers
+
+
+class CursoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Curso
+        fields = ['url', 'nombre', 'creditos']
+
+class ProfesorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Profesor
+        fields = ['url', 'nombre']
+
+class SeccionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Seccion
+        fields = ['url', 'nrc', 'profesor', 'curso', 'especialidad', 'fecha_inicio', 'fecha_fin']
+    
+class BloqueSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Bloque
+        fields = ['url', 'dia_semana', 'hora_inicio', 'hora_fin', 'seccion', 'tipo', 'fecha']
+
+class RequisitoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Requisito
+        fields = ['url', 'curso', 'requisito']
+
+
