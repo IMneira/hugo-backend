c39ac699a4456d8754a0e600761b4e06e9e17a2a
IMneira <imneira@miuandes.cl>
2024-10-20 04:00:35 -0300

    funcionando el filtro de solapamiento

--- hugo_api/api/horarios.py
+++ hugo_api/api/horarios.py
@@ -18,8 +18,22 @@ def get_combinaciones_de_secciones(cursos_ids):
     return combinaciones
 
 def hay_solapamiento(combinacion):
-    #combinacion es una lista de secciones (objetos de la clase Seccion)
-    pass
+    dict_bloques = {dia: [] for dia in range(1, 7)}
+    for seccion in combinacion:
+        for bloque in seccion.bloques.all(): dict_bloques[bloque.dia_semana].append(bloque)
+    for bloques in dict_bloques.values():
+        n = len(bloques)
+        if n > 1:
+            bloques.sort(key=lambda b: b.hora_inicio)
+            for i in range(n - 1):
+                bloque_i = bloques[i]
+                for j in range(i + 1, n):
+                    bloque_j = bloques[j]
+                    if bloque_i.seccion != bloque_j.seccion:
+                        solapan_horas = (bloque_i.hora_inicio < bloque_j.hora_fin and bloque_j.hora_inicio < bloque_i.hora_fin)
+                        solapan_fechas = (bloque_i.fecha_inicio < bloque_j.fecha_fin and bloque_j.fecha_inicio < bloque_i.fecha_fin)
+                        if solapan_horas and solapan_fechas: return True
+    return False
 
 def usa_horario_protegido(combinacion, horarios_protegidos):
     #combinacion es una lista de secciones (objetos de la clase Seccion)
