4b561cd614079849699037757b2fa2bc20928ad8
IMneira <101231123+IMneira@users.noreply.github.com>
2024-11-23 00:29:20 -0300

    actualizado allowed hosts

--- hugo_api/settings.py
+++ hugo_api/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG')
 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'hugo-backend-ucgt.onrender.com']
 
 
 # Application definition
